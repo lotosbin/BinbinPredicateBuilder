3c26e98f762a97297890aed180ca55d42f2da5a7
liubinbin <lotosbin@gmail.com>
2015-03-06 14:57:18 +0800

    fix

---
+++ Binbin.Linq.PredicateBuilder.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Binbin.Linq {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
--- Binbin.Linq.PredicateBuilder/PredicateBuilder.cs
+++ Binbin.Linq.PredicateBuilder/PredicateBuilder.cs
@@ -16,16 +16,52 @@ namespace Binbin.Linq
             return f => false;
         }
 
-        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+        /// <summary>
+        ///     Creates a predicate expression from the specified lambda expression.
+        /// </summary>
+        public static Expression<Func<T, bool>> Create<T>(Expression<Func<T, bool>> predicate)
         {
-            var invoke = Expression.Invoke(right, left.Parameters.Cast<Expression>());
-            return Expression.Lambda<Func<T, bool>>(Expression.OrElse(left.Body, invoke), left.Parameters);
+            return predicate;
         }
 
-        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+        /// <summary>
+        ///     Combines the first predicate with the second using the logical "and".
+        /// </summary>
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
         {
-            var invoke = Expression.Invoke(right, left.Parameters.Cast<Expression>());
-            return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(left.Body, invoke), left.Parameters);
+            return first.Compose(second, Expression.AndAlso);
+        }
+
+        /// <summary>
+        ///     Combines the first predicate with the second using the logical "or".
+        /// </summary>
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.OrElse);
+        }
+
+        /// <summary>
+        ///     Negates the predicate.
+        /// </summary>
+        public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expression)
+        {
+            var negated = Expression.Not(expression.Body);
+            return Expression.Lambda<Func<T, bool>>(negated, expression.Parameters);
+        }
+
+        /// <summary>
+        ///     Combines the first expression with the second using the specified merge function.
+        /// </summary>
+        private static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
+        {
+            // zip parameters (map from parameters of second to parameters of first)
+            var map = first.Parameters.Select((f, i) => new {f, s = second.Parameters[i]}).ToDictionary(p => p.s, p => p.f);
+
+            // replace parameters in the second lambda expression with the parameters in the first
+            var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
+
+            // create a merged lambda expression with parameters from the first expression
+            return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
         }
     }
 }
